b1682c52baa19b811bb6736f01228f196033b0be
ammu703 <ammubadanapuri@gmail.com>
2025-12-26 15:47:41 +0530

    Create large and small.py

---
+++ large and small.py
@@ -0,0 +1,11 @@
+s=[1,4,7,2,10,6,8]
+first_largest=float('-inf')
+second_largest=float('inf')
+for num in s:
+if num>first=largest:
+second_largest=first=largest
+first_largest=num
+elif num<first=largest and 
+num>second_largest:
+second_largest=num
+print(Second_largest)
